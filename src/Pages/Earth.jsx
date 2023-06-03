d5d1b5f8e5e805eef5781c86ccf137a7fa65bf89
shorenatsiklauri <Shorena.tsiklauri1985@gmail.com>
2023-06-03 12:09:07 +0400

    commit4

--- src/Pages/Earth.jsx
+++ src/Pages/Earth.jsx
@@ -3,20 +3,18 @@ import PlanetData from "../data.json";
 import { useParams } from "react-router-dom";
 import "./Pages.css";
 
-
 const PlanetEarth = () => {
-  let {id} =useParams();
+  let { id } = useParams();
   const earthPlanet = PlanetData.find((planet) => planet.name === id);
   console.log(earthPlanet);
-  
- console.log(id);
+
+  console.log(id);
   return (
     <div>
       <PlanetFact planet={earthPlanet} />
     </div>
   );
 };
-
 
 const PlanetFact = ({ planet }) => {
   const [activeTab, setActiveTab] = useState("overview");
@@ -25,45 +23,61 @@ const PlanetFact = ({ planet }) => {
     setActiveTab(tab);
   };
 
+  const [buttonBackground, setButtonBackground] = useState({
+    overview: "",
+    structure: "",
+    geology: ""
+  });
+
+
   return (
     <div>
       <div className="informationalbox">
+
+        <div className="threebuttonbox">
+        <div onClick={() => handleTabClick("overview")}>Overview</div>
+        <div onClick={() => handleTabClick("structure")}>Structure</div>
+        <div onClick={() => handleTabClick("geology")}>Geology</div>
+        </div>
       <div className="boxforpicture">
   {activeTab === "overview" && (
-  <img src={planet.images.planet} alt={`${planet.name} planet`} style={{ width: "450px", height: "450px" }}/>
+  <img className="imgplanets" src={planet.images.planet} alt={`${planet.name} planet` }/>
+  
  
   )}
   {activeTab === "structure" && (
-    <img src={planet.images.internal} alt={`${planet.name} internal`} style={{ width: "450px", height: "450px" }}/>
+    <img  className="imgplanets" src={planet.images.planet} alt={`${planet.name} planet`}/>
   )}
-  {activeTab === "geology" && (
+  {activeTab === "geology"  && (
     <div className="twopictures">
-      <img src={planet.images.planet} alt={`${planet.name} planet`} style={{ width: "450px", height: "450px" }}/>
-      <img src={planet.images.geology} alt={`${planet.name} geology`} style={{ width: "40%", marginLeft: "120px" , marginTop: "-150px"}} />
+      <img className="imgplanets" src={planet.images.planet} alt={`${planet.name} planet`} />
+      <img className="smallimggeology" src={planet.images.geology} alt={`${planet.name} geology`}  />
     </div>
   )}
 </div>
         <div className="boxforsourses">
           <h2>{planet.name}</h2>
-          <div> {activeTab === "overview" ? <div className="textbox"><p>{planet.overview.content}</p></div> : activeTab === "structure" ? <div className="textbox"> <p>{planet.structure.content}</p></div> :<div className="textbox"><p>{planet.geology.content} </p></div>} </div>
-          <div className="boxeslikebutton" style={{ background: `hover ${planet.color}` }}>
-            <div onClick={() => handleTabClick("overview")}>Overview</div>
-          </div>
+          <div>
+          <div> {activeTab === "overview" ? <div className="textbox"><p>{planet.overview.content}</p> <a href={planet.overview.source}>Source: Wikipedia</a> </div> : activeTab === "structure" ? <div className="textbox"> <p>{planet.structure.content}</p>     <a href={planet.structure.source}>Source: Wikipedia</a></div> :<div className="textbox"><p>{planet.geology.content} </p>          <a href={planet.geology.source}>Source: Wikipedia</a></div>} </div>
          
-          <a href={planet.overview.source}>Source: Wikipedia</a>
+         <div className="threeboxeslikebuttons">
+         <div className="boxeslikebutton" style={{ background: activeTab === "overview" ? buttonBackground.overview : "" }}>
+    <div onClick={() => handleTabClick("overview")}>Overview</div>
+  </div>
 
+              
           <div className="boxeslikebutton" style={{ background: `hover ${planet.color}` }}>
-
             <div onClick={() => handleTabClick("structure")}>Structure</div>
           </div>
-       
-          <a href={planet.structure.source}>Source: Wikipedia</a>
-
+             
               <div className="boxeslikebutton" style={{ background: planet.color }}>
             <div onClick={() => handleTabClick("geology")}>Geology</div>
           </div>
+          </div>
        
-          <a href={planet.geology.source}>Source: Wikipedia</a>
+       
+
+        </div>
         </div>
       </div>
       <div className="rotationandtemperature">
--- src/PlanetFacts.jsx
+++ src/PlanetFacts.jsx
@@ -1,34 +1,53 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Route, Routes , Navigate} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route, Routes, Navigate } from "react-router-dom";
 import "./PlanetFacts.css";
 
 import PlanetData from "./data.json";
 import PlanetEarth from "./Pages/Earth";
 
-
 const PlanetFacts = () => {
+  const [activebar, setActivebar] = useState(true);
+
+  const handleBarClick = () => {
+    setActivebar(!activebar);
+
+ 
+  };
+
   return (
     <Router>
       <div>
         <div className="card">
           <div className="headerspace">
-            <h1 style={{ marginLeft: "10px" }}>THE PLANETS</h1>
+            <div className="menue">
+              <h1 style={{ marginLeft: "10px" }}>THE PLANETS</h1>
+              <div className="boxforsvg" onClick={handleBarClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17">
+                  <g fill="#FFF" fillRule="evenodd">
+                    <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
             <nav>
-              <ul className="barbox">
-                {PlanetData.map((planet) => (
-                  <li key={planet.name}>
-                    <Link to={`/planet/${planet.name}`}>{planet.name}</Link>
-                  </li>
-                ))}
-              </ul>
+            <ul className={activebar ? "planetlist show" : "planetlist hide"}>
+  {PlanetData.map((planet) => (
+    <div className="nameandball" key={planet.name}>
+      <div className="smallbolls" style={{ background: planet.color }}></div>
+      <div className="testit">
+  <li style={{ fontSize: "15px", color: "white", textTransform: "uppercase", fontWeight: "700", borderBottom: "none" }}>
+    <Link to={`/planet/${planet.name}`}>{planet.name}</Link>
+  </li>
+</div>
+    </div>
+  ))}
+</ul>
             </nav>
           </div>
           <div className="planetspace">
             <Routes>
-            
-            <Route path="/" element={<Navigate to="/planet/Mercury"/>} />
+              <Route path="/" element={<Navigate to="/planet/Mercury" />} />
               <Route path="/planet/:id" element={<PlanetEarth />} />
-    
             </Routes>
           </div>
         </div>
